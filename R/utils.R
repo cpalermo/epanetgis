18fd925c5ad91e7bc8e44476b826ace3141b889a
Carlo Palermo <carlo.palermo@hdati.com>
2019-11-01 18:36:39 +0100

    custom get_layer in utils

--- R/utils.R
+++ R/utils.R
@@ -7,37 +7,49 @@ colnames_force <- function(df, col_names) {
   return(res)
 }
 
-#' Force MULTILINESTRING in LINESTRING
+#' Custom st_read
+#'
+#' @export
+get_layer <- function(dsn, layer, crs) {
+  ll <- sf::st_read(dsn, layer)
+  ll <- sf::st_zm(ll)
+  ll <- sf::st_transform(ll, crs= crs)
+  return(ll)
+}
+
+#' Force MULTILINESTRING to LINESTRING
 #'
 #' @export
 multi2linestring <- function(layer) {
-  layer <- st_zm(layer)
-  for( i in 1 : dim(layer)[1] ) {
-    df <- layer[i, ]
-    gg <- st_geometry(df)
-    ls <- st_cast(gg, "LINESTRING")
-    if( length(ls) > 2 ) {
-      stop("Can handle MULTILINESTRING with length =< 2")
-    } else {
-      if( length(ls) > 1 ) {
-        sm <- st_line_sample(ls, sample = c(0, 1))
-        sc <- st_coordinates(sm)
-        ##
-        coords <- st_coordinates(df)
-        xy <- coords[, 1:2]
-        l1 <- coords[, "L1"]
-        ## Check if the end of first line intersect the start of the second line
-        if( sum(abs(sc[2, 1:2] - sc[3, 1:2])) < 0.1 ) {
-          new_ls <- st_geometry(st_linestring(xy))
-        } else {
-          ## If not, invert order:
-          cc <- rbind(xy[l1 == 2, ], xy[l1 == 1, ])
-          new_ls <- st_geometry(st_linestring(cc))
+  gtype <- table(st_geometry_type(layer))
+  if(gtype["MULTILINESTRING"] != 0) {
+    for( i in 1 : dim(layer)[1] ) {
+      df <- layer[i, ]
+      gg <- st_geometry(df)
+      ls <- st_cast(gg, "LINESTRING")
+      if( length(ls) > 2 ) {
+        stop("Can handle MULTILINESTRING with length =< 2")
+      } else {
+        if( length(ls) > 1 ) {
+          sm <- st_line_sample(ls, sample = c(0, 1))
+          sc <- st_coordinates(sm)
+          ##
+          coords <- st_coordinates(df)
+          xy <- coords[, 1:2]
+          l1 <- coords[, "L1"]
+          ## Check if the end of first line intersect the start of the second line
+          if( sum(abs(sc[2, 1:2] - sc[3, 1:2])) < 0.1 ) {
+            new_ls <- st_geometry(st_linestring(xy))
+          } else {
+            ## If not, invert order:
+            cc <- rbind(xy[l1 == 2, ], xy[l1 == 1, ])
+            new_ls <- st_geometry(st_linestring(cc))
+          }
+          ls <- new_ls
         }
-        ls <- new_ls
       }
+      st_geometry(layer[i, ]) <- ls
     }
-    st_geometry(layer[i, ]) <- ls
   }
   return(layer)
 }
