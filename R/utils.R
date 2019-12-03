7e92f27c39884d4c052fa204e12a72542ca8214e
Carlo Palermo <carlo.palermo@hdati.com>
2019-12-03 11:30:55 +0100

    Add utils functions

--- R/utils.R
+++ R/utils.R
@@ -53,3 +53,42 @@ multi2linestring <- function(layer) {
   }
   return(layer)
 }
+
+#' sample split point
+#'
+#' @export
+sample_split_point <- function(point_near, link_near, min_dist) {
+  ## Find a split point
+  link_sample <- st_line_sample(link_near, n = st_length(link_near))
+  link_points <- st_cast(link_sample, "POINT")
+  dd <- as.numeric(st_distance(point_near, link_points))
+  mm <- which(dd > min_dist)
+  split_i <- mm[which.min(dd[mm])]
+  point_split <- link_points[split_i]
+  return(point_split)
+}
+
+#' sample split link
+#'
+#' @export
+sample_split_link <- function(point_split, link_near) {
+  link_snap <- st_snap(link_near, point_split, tol=1e-9)
+  link_split <- st_split(link_snap, point_split)
+  link_split <- st_cast(st_geometry(link_split), do_split=TRUE)
+  return(link_split)
+}
+
+#' Link Nodes
+#'
+#' @export
+link_nodes <-  function(layer, nodes) {
+  start_nd <- st_line_sample(layer, sample = 0)
+  end_nd <- st_line_sample(layer, sample = 1)
+  ##
+  start_d <- st_distance(start_nd, nodes)
+  end_d <- st_distance(end_nd, nodes)
+  start_i <- apply(start_d, 1, which.min)
+  end_i <- apply(end_d, 1, which.min)
+  ##
+  data.frame(start_i, end_i)
+}
